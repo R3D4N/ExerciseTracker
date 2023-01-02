d3704731a5441fc422eebec0e871017e50bf3673
R3_D4N <bellidoangelo97@gmail.com>
2023-01-02 02:10:04 -0500

    Created conection and schemas of database file database.js

---
+++ database.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('strictQuery', false);
+
+const exerciseSchema = new mongoose.Schema({
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    description: String,
+    duration: Number,
+    date: String
+});
+
+const userSchema = new mongoose.Schema({
+    username: String
+});
+
+const logSchema = new mongoose.Schema({
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    count: Number,
+    log: [{type: mongoose.Schema.Types.ObjectId, ref: 'Exercise'}]
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+const User = mongoose.model('User', userSchema);
+const Log = mongoose.model('Log', logSchema);
+
+module.exports = {Exercise, User, Log};
