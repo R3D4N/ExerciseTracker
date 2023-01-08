49c3a5c79acb00a03418585a3ece889111a951d8
R3_D4N <bellidoangelo97@gmail.com>
2023-01-08 02:48:33 -0500

    Correction of schema in database.js, now just one schema called data

--- database.js
+++ database.js
@@ -4,26 +4,16 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('strictQuery', false);
 
-const exerciseSchema = new mongoose.Schema({
-    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    description: String,
-    duration: Number,
-    date: String
+const dataSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    count: {type: Number, default: 0},
+    log: [{
+        description: String,
+        duration: Number,
+        date: String
+    }]
 });
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    _id: mongoose.Schema.Types.ObjectId
-});
+const Data = mongoose.model('Data', dataSchema);
 
-const logSchema = new mongoose.Schema({
-    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    count: Number,
-    log: [{description: String, duration: Number, date: String}]
-});
-
-const Exercise = mongoose.model('Exercise', exerciseSchema);
-const User = mongoose.model('User', userSchema);
-const Log = mongoose.model('Log', logSchema);
-
-module.exports = {Exercise, User, Log};
+module.exports = {Data};
